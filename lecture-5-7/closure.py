a944ce1378466f4130354b73c79178044b4f7c8d
Alex Ratner <ajratner@gmail.com>
2015-10-02 16:35:33 -0700

    Adding 5-7, 4 solutions

---
+++ lecture-5-7/closure.py
@@ -0,0 +1,73 @@
+# A  = set(["name", "category"]) # These are the attribute set.
+# fds = [ (set(["name"]),"color"),
+#         (set(["category"]), "department"),
+#         (set(["color", "category"]), "price") ]
+
+def to_set(x):
+  """Convert input int, string, list, tuple, set -> set"""
+  if type(x) == set:
+    return x
+  elif type(x) in [list, set]:
+    return set(x)
+  elif type(x) in [str, int]:
+    return set([x])
+
+def fd_to_str((lhs,rhs)): return ",".join(to_set(lhs)) + " -> " + ",".join(to_set(rhs))
+
+def fds_to_str(fds): return "\n\t".join(map(fd_to_str, fds))
+
+def set_to_str(x): return "{" + ",".join(x) + "}"
+
+def print_setup(A, fds):
+  print("Attributes = " + set_to_str(A))
+  print("FDs = \t" + fds_to_str(fds))
+
+"""Does the FD apply"""
+def fd_applies(x, (lhs,rhs)): return to_set(lhs).issubset(x)
+
+def compute_closure(x, fds, verbose=False):
+    """Compute X^{+} wrt to the given FDS"""
+    bChanged = True
+    x_ret    = x.copy()
+    while bChanged:
+        bChanged = False # Must change on each iteration
+        for fd in fds: 
+            (lhs, rhs) = map(to_set, fd) # lhs -> rhs
+            if fd_applies(x_ret, fd) and not rhs.issubset(x_ret):
+                x_ret = x_ret.union(rhs)
+                if verbose:
+                    print("Using FD " + fd_to_str(fd))
+                    print("\t Updated x to " + set_to_str(x_ret))
+                bChanged = True
+    return x_ret
+
+def is_fd_implied(fds, lhs, rhs, verbose=False):
+    """Check if lhs -> rhs is implied by the given set of fds"""
+    xp = compute_closure(lhs,fds,verbose=verbose)
+    if verbose: print(set_to_str(lhs) +"+ = "+ set_to_str(xp))
+    return to_set(rhs).issubset(xp)
+
+def is_superkey(A,B,fds, verbose=False):
+    """Check if A is a super key in B according to the fds"""
+    return is_fd_implied(fds, A, B)
+
+import itertools
+def is_key(A,B,fds,verbose=False):
+    """Check if A is a key in B wrt to fds"""
+    m=len(A)
+    subsets = set(itertools.combinations(A, m-1))
+    return is_superkey(A,B,fds) and all(not is_superkey(set(SA),B,fds) for SA in subsets)
+
+#
+# Key example from lecture
+#
+def key_example():
+    xmC=set(["A","B"])
+    xmB=set(["A","C"])
+    xmA=set(["B","C"])
+    B  =set(["A","B","C"])
+
+    fd1=(xmC,"C"); fd2=(xmB,"B"); fd3=(xmA,"A")
+    fds=[fd1,fd2,fd3]
+
+    return is_key(xmA,B,fds) and is_key(xmB,B,fds) and is_key(xmC,B,fds)
